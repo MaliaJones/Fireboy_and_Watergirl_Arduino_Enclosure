f8f59b831a945c7069890bed64782a68b95da1c2
Malia Jones <89104858+MaliaJones@users.noreply.github.com>
2026-04-05 14:33:40 -0500

    Updated code to: level2 done

--- fboy_wgirl_project2.ino
+++ fboy_wgirl_project2.ino
@@ -1,4 +1,3 @@
-// Meow meow kitty cat
 // imports
 #include <Servo.h>
 
@@ -8,14 +7,21 @@ Servo servo1_360;
 Servo servo2_180;
 Servo servo3_360;
 Servo servo4_180;
-int s1pin = 5;
+int s1pin = 12;
 int s2pin = 3;
 int s3pin = 11;
-int s4pin;
+int s4pin = 4;
 // LEDs
 int rgbled_Rpin = 11;
 int rgbled_Gpin = 9;
 int rgbled_Bpin = 10;
+// Copper Tape Switches
+int switch2pin = A1;
+
+
+// Other
+int prevState = LOW;
+int currState = LOW;
 
 
 
@@ -25,6 +31,8 @@ void setup() {
   Serial.begin(9600);
   // Servos
   servo1_360.attach(s1pin);
+    servo1_360.write(90);
+    delay(1000);
   servo2_180.attach(s2pin);
   servo3_360.attach(s3pin);
   servo4_180.attach(s4pin);
@@ -32,11 +40,11 @@ void setup() {
   pinMode(rgbled_Rpin, OUTPUT);
   pinMode(rgbled_Gpin, OUTPUT);
   pinMode(rgbled_Bpin, OUTPUT);
+  // Copper Tape Switches
+  pinMode(switch2pin, INPUT);
 
+  CloseDoor(200, servo1_360);
 
-  //OpenDoor(2000, servo3_360);
-  //delay(1000);
-  //CloseDoor(2000, servo3_360);
 
   //servo2.write(70);
   //delay(1000);
@@ -45,17 +53,51 @@ void setup() {
 }
 
 void loop() {
+  // Room2 switches
+  currState = digitalRead(switch2pin);
+  if (prevState != currState){
+    if (currState == HIGH){
+      Serial.println("switch2: HIGH");
+      OpenDoor(1800, servo1_360);
+      delay(2000);
+    } else{
+      Serial.println("switch2: LOW");
+      CloseDoor(1800, servo1_360);
+      delay(2000);
+    }
+    prevState = currState;
+  }
 
+  // Room3 switch
+
+
+
+}
+
+void SeeSawPosition(int val){
+  if (val < 600){
+    currStateSS = "DIP RIGHT";
+  } else if (val < 750){
+    currStateSS = "MIDDLE";
+  } else{
+    currStateSS = "DIP LEFT";
+  }
+  delay (100);
+
+  if (prevStateSS != currStateSS){
+    Serial.println(currStateSS);
+    prevStateSS = currStateSS;
+  }
 }
 
 // opens door for servo s taking x seconds, with set speed
-void OpenDoor(int amt, Servo s){
+void OpenDoor(int amt, Servo &s){
   s.write(80);
   delay(amt);
-  s.write(95);
+  s.write(90);
 }
 // closes door for servo s taking x seconds, with set speed
-void CloseDoor(int amt, Servo s){
+void CloseDoor(int amt, Servo &s){
   s.write(100);
   delay(amt);
   s.write(90);
